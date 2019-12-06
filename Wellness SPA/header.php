e66c9e1b446d565ddcfab64e7d1b5c27abac1dd3
marcopuglisi <58331367+marcopuglisi@users.noreply.github.com>
2019-12-06 14:05:00 +0100

    Aggiunto bottone registrati

--- Wellness SPA/header.php
+++ Wellness SPA/header.php
@@ -2,6 +2,7 @@
 <head>
 </head>
 <body>
+  <div class="container">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Wellness SPA</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,12 +11,17 @@
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
       <a class="nav-item nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link px-3" href="#">Centri Benessere</a>
-      <a class="nav-item nav-link px-3" href="#">Palestre</a>
-      <a class="nav-item nav-link px-3" href="#">SPA</a>
-      <a class="nav-item nav-link px-3" href="#">Centri Termali</a>
+      <a class="nav-item nav-link mx-1" href="#">Centri Benessere</a>
+      <a class="nav-item nav-link mx-1 " href="#">Palestre</a>
+      <a class="nav-item nav-link mx-1" href="#">SPA</a>
+      <a class="nav-item nav-link mx-1" href="#">Centri Termali</a>
+      <a class="nav-item nav-link mx-1" href="#">Galleria</a>
+      <a class="nav-item nav-link mx-1" href="#">Contatti</a>
+      <a class="nav-item nav-link mr-5 ml-1" href="#">Chi Siamo</a>
+      <button type="button" class="ml-5 btn btn-dark" href="signup.php">Registrati</button>
     </div>
-  </div>
 </nav>
+</div>
 </body>
 </html>
+
