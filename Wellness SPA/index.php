cc436bab9aa23711aa4099f530865f791e0c0258
marcopuglisi <58331367+marcopuglisi@users.noreply.github.com>
2020-01-17 14:00:49 +0100

    Modifiche alla homepage

--- Wellness SPA/index.php
+++ Wellness SPA/index.php
@@ -41,44 +41,119 @@ include 'header.php';
 </div>
 </div>
 <br>
-<div class="card-deck">
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+			<div class="card-deck mx-3">
 
-		<div class="card">
-		<img src="images/spa.jpg" class="card-img" alt="..." >
-	  <div class="card-img-overlay">
-	    <h3 class="card-title text-light text-center">BUSINESS SPA</h3>
-			<a href="#" class="btn btn-light">Acquista</a>
-	  </div>
+					<div class="card">
+					<img src="images/spa.jpg" class="card-img" alt="..." >
+				  <div class="card-img-overlay">
+				    <h3 class="card-title text-light text-center">BUSINESS SPA</h3>
+				  </div>
+			    </div>
+
+
+					<div class="card" >
+					<img src="images/spa.jpg" class="card-img" alt="..."  >
+				  <div class="card-img-overlay">
+				    <h3 class="card-title text-light text-center">BUSINESS SPA</h3>
+				  </div>
+			    </div>
+
+
+					<div class="card">
+					<img src="images/spa.jpg" class="card-img" alt="..." >
+				  <div class="card-img-overlay">
+				    <h3 class="card-title text-light text-center">BUSINESS SPA</h3>
+				  </div>
+			    </div>
+
+			</div>
     </div>
+    <div class="carousel-item">
+			<div class="card-deck mx-3">
+
+					<div class="card">
+					<img src="images/spa.jpg" class="card-img" alt="..." >
+				  <div class="card-img-overlay">
+				    <h3 class="card-title text-light text-center">BUSINESS SPA</h3>
+				  </div>
+			    </div>
 
 
-		<div class="card" >
-		<img src="images/spa.jpg" class="card-img" alt="..."  >
-	  <div class="card-img-overlay">
-	    <h3 class="card-title text-light text-center">BUSINESS SPA</h3>
-			<a href="#" class="btn btn-light">Acquista</a>
-	  </div>
+					<div class="card" >
+					<img src="images/spa.jpg" class="card-img" alt="..."  >
+				  <div class="card-img-overlay">
+				    <h3 class="card-title text-light text-center">BUSINESS SPA</h3>
+				  </div>
+			    </div>
+
+
+					<div class="card">
+					<img src="images/spa.jpg" class="card-img" alt="..." >
+				  <div class="card-img-overlay">
+				    <h3 class="card-title text-light text-center">BUSINESS SPA</h3>
+				  </div>
+			    </div>
+
+			</div>
     </div>
+    <div class="carousel-item">
+			<div class="card-deck mx-3">
+
+					<div class="card">
+					<img src="images/spa.jpg" class="card-img" alt="..." >
+				  <div class="card-img-overlay">
+				    <h3 class="card-title text-light text-center">BUSINESS SPA</h3>
+				  </div>
+			    </div>
 
 
-		<div class="card">
-		<img src="images/spa.jpg" class="card-img" alt="..." >
-	  <div class="card-img-overlay">
-	    <h3 class="card-title text-light text-center">BUSINESS SPA</h3>
-			<a href="#" class="btn btn-light">Acquista</a>
-	  </div>
+					<div class="card" >
+					<img src="images/spa.jpg" class="card-img" alt="..."  >
+				  <div class="card-img-overlay">
+				    <h3 class="card-title text-light text-center">BUSINESS SPA</h3>
+				  </div>
+			    </div>
+
+
+					<div class="card">
+					<img src="images/spa.jpg" class="card-img" alt="..." >
+				  <div class="card-img-overlay">
+				    <h3 class="card-title text-light text-center">BUSINESS SPA</h3>
+				  </div>
+			    </div>
+
+			</div>
     </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<div class="container-fluid">
+	<div class="row">
+	<div class="col-8">
+		<p class="font-family: Nunito,sans-serif; float-left text-light">
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dictum metus non magna egestas porttitor. Cras sit amet dictum magna, a lacinia augue. Sed rhoncus pulvinar aliquet. In hac habitasse platea dictumst. Suspendisse potenti. Pellentesque malesuada tincidunt risus, semper pulvinar mi dignissim nec. Quisque arcu metus, tincidunt in eleifend a, varius ut velit. Cras pellentesque aliquet turpis in luctus. Phasellus ac interdum urna. Proin neque est, blandit sit amet feugiat hendrerit,
+	commodo at nisi. Phasellus sed rhoncus augue. Vivamus quis consectetur ipsum. Maecenas euismod neque felis, at sollicitudin tellus vehicula quis. Mauris sed ligula vel diam mattis feugiat ut a urna.</p>
+
+	</div>
+	<div class="col-4"><h1>TITOLO 1</h1></div>
+	<div class="row">
+	<div class="col-8"><h1>TITOLO 1</h1></div>
+	<div class="col-4">
+		<p class="font-family: Nunito,sans-serif; float-left text-light">
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dictum metus non magna egestas porttitor. Cras sit amet dictum magna, a lacinia augue. Sed rhoncus pulvinar aliquet. In hac habitasse platea dictumst. Suspendisse potenti. Pellentesque malesuada tincidunt risus, semper pulvinar mi dignissim nec. Quisque arcu metus, tincidunt in eleifend a, varius ut velit. Cras pellentesque aliquet turpis in luctus. Phasellus ac interdum urna. Proin neque est, blandit sit amet feugiat hendrerit,
+	commodo at nisi. Phasellus sed rhoncus augue. Vivamus quis consectetur ipsum. Maecenas euismod neque felis, at sollicitudin tellus vehicula quis. Mauris sed ligula vel diam mattis feugiat ut a urna.</p>
 
 </div>
-	<div class="row">
-		<div class="col"><br>
-	<p class="font-family: Nunito,sans-serif; float-left text-light">
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dictum metus non magna egestas porttitor. Cras sit amet dictum magna, a lacinia augue. Sed rhoncus pulvinar aliquet. In hac habitasse platea dictumst. Suspendisse potenti. Pellentesque malesuada tincidunt risus, semper pulvinar mi dignissim nec. Quisque arcu metus, tincidunt in eleifend a, varius ut velit. Cras pellentesque aliquet turpis in luctus. Phasellus ac interdum urna. Proin neque est, blandit sit amet feugiat hendrerit,
-commodo at nisi. Phasellus sed rhoncus augue. Vivamus quis consectetur ipsum. Maecenas euismod neque felis, at sollicitudin tellus vehicula quis. Mauris sed ligula vel diam mattis feugiat ut a urna.</p>
-</div><br>
-		<div class="col"><br><br><br><br><br><br><br><br><br><br><br><br>
-	<p class="font-family: Nunito,sans-serif; float-right text-light">
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dictum metus non magna egestas porttitor. Cras sit amet dictum magna, a lacinia augue. Sed rhoncus pulvinar aliquet. In hac habitasse platea dictumst. Suspendisse potenti. Pellentesque malesuada tincidunt risus, semper pulvinar mi dignissim nec. Quisque arcu metus, tincidunt in eleifend a, varius ut velit. Cras pellentesque aliquet turpis in luctus. Phasellus ac interdum urna. Proin neque est, blandit sit amet feugiat hendrerit, commodo at nisi. Phasellus sed rhoncus augue. Vivamus quis consectetur ipsum. Maecenas euismod neque felis, at sollicitudin tellus vehicula quis. Mauris sed ligula vel diam mattis feugiat ut a urna.</p>
 	</div>
 </div>
 </div>
